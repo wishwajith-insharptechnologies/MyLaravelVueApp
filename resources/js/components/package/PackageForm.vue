dc4119957a7812f84e1e98437355a5601b3794ef
wishwajith <vishwajithm@insharptechnologies.com>
2024-07-10 02:14:08 +0530

    package validation

--- resources/js/components/package/PackageForm.vue
+++ resources/js/components/package/PackageForm.vue
@@ -1,79 +1,122 @@
 <template>
-    <a-form
-      :model="form"
-      layout="vertical"
-      @submit.prevent="handleSubmit"
+  <a-form
+    :model="form"
+    layout="vertical"
+    ref="formRef"
+    @finish="onFinish"
+  >
+    <a-form-item
+      label="Title"
+      :name="['form', 'title']"
+      :rules="[{ required: true, message: 'Title is required', trigger: 'blur' }]"
     >
-      <a-form-item label="Title">
-        <a-input v-model:value="form.title" placeholder="Enter title" />
-      </a-form-item>
+      <a-input v-model:value="form.title" placeholder="Enter title" />
+    </a-form-item>
 
-      <a-form-item label="Description">
-        <a-input v-model:value="form.description" placeholder="Enter description" />
-      </a-form-item>
+    <a-form-item
+      label="Description"
+      :name="['form', 'description']"
+      :rules="[{ required: true, message: 'Description is required', trigger: 'blur' }]"
+    >
+      <a-input v-model:value="form.description" placeholder="Enter description" />
+    </a-form-item>
 
-      <a-form-item label="Project">
-          <a-select v-model:value="form.product_id"   @change="projectSelectChange" placeholder="Select product ID">
-          <a-select-option v-for="product in projectsList" :key="product.id" :value="product.id">
-            {{ product.project_name }}
-          </a-select-option>
-        </a-select>
-        </a-form-item>
+    <a-form-item
+      label="Project"
+      :name="['form', 'project_id']"
+      :rules="[{ required: true, message: 'Product is required', trigger: 'change' }]"
+    >
+      <a-select v-model:value="form.product_id" @change="projectSelectChange" placeholder="Select product ID">
+        <a-select-option v-for="product in projectsList" :key="product.id" :value="product.id">
+          {{ product.project_name }}
+        </a-select-option>
+      </a-select>
+    </a-form-item>
 
-      <a-form-item label="Rank">
-        <a-input v-model:value="form.rank" placeholder="Enter rank" />
-      </a-form-item>
+    <a-form-item
+      label="Rank"
+      :name="['form', 'rank']"
+      :rules="[{ required: true, message: 'Rank is required', trigger: 'blur' }]"
+    >
+      <a-input v-model:value="form.rank" placeholder="Enter rank" />
+    </a-form-item>
 
-      <a-form-item label="Validity">
-        <a-input v-model:value="form.validity" placeholder="Enter validity period" />
-      </a-form-item>
+    <a-form-item
+      label="Validity"
+      :name="['form', 'validity']"
+      :rules="[{ required: true, message: 'Validity period is required', trigger: 'blur' }]"
+    >
+      <a-input v-model:value="form.validity" placeholder="Enter validity period" />
+    </a-form-item>
 
-      <a-form-item label="Price">
-        <a-input-number v-model:value="form.price" placeholder="Enter price" style="width: 100%;" />
-      </a-form-item>
+    <a-form-item
+      label="Price"
+      :name="['form', 'price']"
+      :rules="[{ required: true, type: 'number', message: 'Price is required', trigger: 'blur' }]"
+    >
+      <a-input-number v-model:value="form.price" placeholder="Enter price" style="width: 100%;" />
+    </a-form-item>
 
-      <a-form-item label="Discount">
-        <a-input-number v-model:value="form.discount" placeholder="Enter discount" style="width: 100%;" />
-      </a-form-item>
+    <a-form-item
+      label="Discount"
+      :name="['form', 'discount']"
+      :rules="[{ type: 'number', message: 'Discount is required', trigger: 'blur' }]"
+    >
+      <a-input-number v-model:value="form.discount" placeholder="Enter discount" style="width: 100%;" />
+    </a-form-item>
 
-      <a-form-item label="Images">
-        <a-upload
-          v-model:file-list="form.images"
-          list-type="picture"
-          action="/upload.do"
-          :multiple="true"
-        >
-          <a-button>
-            <a-icon type="upload" /> Upload
-          </a-button>
-        </a-upload>
-      </a-form-item>
+    <a-form-item
+      label="Images"
+      :name="['form', 'images']"
+      :rules="[{ required: true, message: 'Please upload at least one image', trigger: 'change' }]"
+    >
+      <a-upload
+        v-model:file-list="form.images"
+        list-type="picture"
+        action="/upload.do"
+        :multiple="true"
+      >
+        <a-button>
+          <a-icon type="upload" /> Upload
+        </a-button>
+      </a-upload>
+    </a-form-item>
 
-      <a-form-item label="Status">
-        <a-switch v-model:checked="form.status" />
-      </a-form-item>
+    <a-form-item
+      label="Status"
+      :name="['form', 'status']"
+      :rules="[{ required: true, type: 'boolean', message: 'Status is required', trigger: 'change' }]"
+    >
+      <a-switch v-model:checked="form.status" />
+    </a-form-item>
 
-      <a-form-item label="Trial Period">
-        <a-input v-model:value="form.trial_period" placeholder="Enter trial period" />
-      </a-form-item>
+    <a-form-item
+      label="Trial Period"
+      :name="['form', 'trial_period']"
+      :rules="[{ type: 'number', message: 'Trial period is required', trigger: 'blur' }]"
+    >
+      <a-input v-model:value="form.trial_period" placeholder="Enter trial period" />
+    </a-form-item>
 
-      <a-form-item label="Category ID">
-        <a-input v-model:value="form.category_id" placeholder="Enter category ID" />
-      </a-form-item>
+    <a-form-item
+      label="Category ID"
+      :name="['form', 'category_id']"
+      :rules="[{ required: true, message: 'Category ID is required', trigger: 'blur' }]"
+    >
+      <a-input v-model:value="form.category_id" placeholder="Enter category ID" />
+    </a-form-item>
 
-      <LimitationForm
-          v-model="form.limitation"
-          :limitations="loadedLimitations"
-          @update:value="handleLimitationsUpdate"
-          @update:errors="handleLimitationsErrors"
-        />
+    <LimitationForm
+      v-model="form.limitation"
+      :limitations="loadedLimitations"
+      @update:value="handleLimitationsUpdate"
+      @update:errors="handleLimitationsErrors"
+    />
 
-      <a-form-item>
-        <a-button type="primary" html-type="submit">Submit</a-button>
-      </a-form-item>
-    </a-form>
-
-
+    <a-form-item>
+      <a-button type="primary" html-type="submit">Submit</a-button>
+    </a-form-item>
+  </a-form>
   </template>
 
   <script setup>
@@ -103,6 +146,7 @@
     category_id: '',
     limitation: {},
   });
+  const formRef = ref(null);
   const errors = ref([]);
   const limitationErrors = ref(false);
   const submitting = ref(false);
@@ -113,17 +157,34 @@ onMounted(() => {
     getProjectList();
 });
 
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    email: '${label} is not a valid email!',
+    number: '${label} is not a valid number!',
+  },
+  number: {
+    range: '${label} must be between ${min} and ${max}',
+  },
+};
+
+const onFinish = values => {
+  console.log('Success:', values);
+};
+
 const handleSubmit = async () => {
-  errors.value = null;
-  submitting.value = true;
 
-  if (!props.package) {
-    await submitForm();
-  } else {
-    await updatePackage();
-  }
+    await formRef.value.validate();
+    errors.value = null;
+    submitting.value = true;
 
-  submitting.value = false;
+    if (!props.package) {
+        await submitForm();
+    } else {
+        await updatePackage();
+    }
+
+    submitting.value = false;
 };
 
 const getProjectList = async () => {
