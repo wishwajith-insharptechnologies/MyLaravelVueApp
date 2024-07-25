f688b34c428e267756f570f2e424bd057c8974c8
wishwajith <vishwajithm@insharptechnologies.com>
2024-07-25 12:24:51 +0530

    add user seed file

---
+++ database/seeders/FirstUserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class FirstUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'id' => 20,
+            'name' => 'super admin',
+            'last_name' => 'last name',
+            'email' => 'superadmin@superadmin.com',
+            'email_verified_at' => null,
+            'password' =>  Hash::make(12345678),
+            'remember_token' => null,
+            'theme_dark' => null,
+        ]);
+    }
+}
